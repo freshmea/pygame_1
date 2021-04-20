ad41b6705f9f464bea1f2d282702961939e8375b
freshmea <freshmea@gmail.com>
2021-04-20 22:30:37 +0900

    움직이는 마이크

--- venv/main.py
+++ venv/main.py
@@ -1,4 +1,4 @@
-import pygame, sys, random
+import pygame, sys, random, time
 from pygame.locals import *
 pygame.init()
 pygame.display.set_caption("비 맞는 마이크")
@@ -8,10 +8,12 @@ screen_y =480*2
 screen = pygame.display.set_mode((screen_x, screen_y))
 
 clock = pygame.time.Clock()
-raindrop_spawn_time=0
+raindrop_spawn_time = 0
+last_hit_time = 0
 rain_quantity=11 #구름 양
 
 mike_umbrella_image = pygame.image.load("images/Mike_umbrella.png").convert()
+mike_image = pygame.image.load(("images/mike.png")).convert()
 cloud_image = pygame.image.load(("images/cloud.png")).convert()
 
 """구름 클래스 정의"""
@@ -22,7 +24,7 @@ class Cloud:
     def move(self):
         if self.x >= screen_x:
             self.x = 1
-        self.x += random.randint(1,10)
+        self.x += 3
     def rain(self):
         raindrops.append(Raindrop(random.randint(self.x, self.x+300), self.y+100))
     def draw(self):
@@ -56,7 +58,10 @@ class Mike:
     def hit_by(self, raindrop):
         return pygame.Rect(self.x, self.y, 170, 192).collidepoint((raindrop.x, raindrop.y))
     def draw(self):
-        screen.blit(mike_umbrella_image,(self.x, self.y))
+        if time.time() > last_hit_time+.1:
+            screen.blit(mike_image, (self.x, self.y))
+        else:
+            screen.blit(mike_umbrella_image,(self.x, self.y))
 
 """Raindrop 클래스 정의"""
 class Raindrop:
@@ -106,11 +111,22 @@ while True:
     cloud2.move()
     cloud.draw()
     cloud2.draw()
+
     """빗방울 그리기"""
     while i<len(raindrops):
         raindrops[i].move()
         raindrops[i].draw()
-        if raindrops[i].off_screen() or mike.hit_by(raindrops[i]):
+        flag = False
+        if raindrops[i].off_screen():
+            del raindrops[i]
+            i -= 1
+            flag = True
+        if mike.hit_by(raindrops[i]):
+            del raindrops[i]
+            last_hit_time = time.time()
+            i -= 1
+            flag = True
+        if flag:
             del raindrops[i]
             i -= 1
         i+=1

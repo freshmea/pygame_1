c48a667c0d5b3da5dc918a651277f85edb08d241
freshmea <freshmea@gmail.com>
2021-04-29 01:39:12 +0900

    지렁이 게임

--- venv/slither.py
+++ venv/slither.py
@@ -1,15 +1,19 @@
+import random
+
 import pygame
 import math
+import sys
 from pygame.locals import *
 
 vec = pygame.math.Vector2
 
-WIDTH = 640*2
+WIDTH = 640*3
 HEIGHT = 480*2
 TITLE = 'withpaint'
 BLACK = (0,0,0)
 YELLOW = (125, 125, 0)
 WHITE = (255,255,255)
+RED = (255, 0, 0)
 FPS = 60
 SLITHER_SPEED = 5
 START_SCORE = 100
@@ -25,12 +29,16 @@ class Game:
         pygame.display.set_caption(TITLE)
         self.load_data()
         self.slither = Slither(self)
+        self.pizzas = []
+        self.spwntime = 0
+
     def load_data(self):
         self.background = pygame.image.load('images/Stars.png').convert_alpha()
         self.background = pygame.transform.scale(self.background, (WIDTH, HEIGHT))
 
     def new(self):
         self.slither = Slither(self)
+        self.pizzas = []
 
     def run(self):
         self.playing = True
@@ -42,8 +50,21 @@ class Game:
 
     def update(self):
         self.slither.update()
+
+        #지렁이 충돌 확인
         if self.slither.crash():
             self.playing = False
+
+        # 피자 생산
+        if pygame.time.get_ticks()-self.spwntime > 1000 and len(self.pizzas) < 10 :
+            self.pizzas.append(Pizza(self))
+            self.spwntime = pygame.time.get_ticks()
+
+        #지렁이가 피자 먹었는지 확인
+        for i in self.pizzas:
+            if self.slither.eat(i):
+                self.pizzas.remove(i)
+                self.slither.score += 50
 
     def events(self):
         for event in pygame.event.get():
@@ -58,13 +79,15 @@ class Game:
         self.screen.fill(BLACK)
         self.screen.blit(self.background, (0, 0))
         self.slither.draw()
+        for i in self.pizzas:
+            i.draw()
         self.draw_text(f"점수:{self.slither.score}  남은 시간: 스테이지",22, WHITE, WIDTH / 2, 15)
         pygame.display.update()
 
     def show_start_screen(self):
         self.screen.fill(BLACK)
         self.draw_text(TITLE, 48, WHITE, WIDTH / 2, HEIGHT / 4)
-        self.draw_text("위드 페인트 게임을 시작합니다. ", 22, WHITE, WIDTH / 2, HEIGHT / 2)
+        self.draw_text("지렁이 게임을 시작합니다. ", 22, WHITE, WIDTH / 2, HEIGHT / 2)
         self.draw_text("스페이스키를 누루면 시작합니다. ", 22, WHITE, WIDTH / 2, HEIGHT * 3 / 4)
         self.draw_text("1, 2 :색 바꿈 설정         3, 4: 굵기 설정  esc: 지우기", 22, WHITE, WIDTH / 2, 15)
         pygame.display.update()
@@ -125,11 +148,22 @@ class Slither:
     def crash(self):
         crash = False
         for i in self.data:
-            print(len(self.data)-self.data.index(i))
             if len(self.data)-self.data.index(i) > 10:
                 crash = pygame.Rect(self.data[-1].x, self.data[-1].y, 10, 10).colliderect(pygame.Rect(i.x, i.y, 10,10))
                 if crash == True:
                     return crash
+
+    def eat(self, pizza):
+        return pygame.Rect(self.data[-1].x-5, self.data[-1].y-5, 10, 10).colliderect(pygame.Rect(pizza.pos.x-20, pizza.pos.y-20, 40, 40))
+
+class Pizza:
+    def __init__(self, game):
+        self.game = game
+        self.pos = vec(random.randint(0, WIDTH), random.randint(0, HEIGHT))
+        self.color = RED
+
+    def draw(self):
+        pygame.draw.circle(self.game.screen, self.color, self.pos, 40)
 
 
 game = Game()
